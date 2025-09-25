30aee5312a4e6947649baba321de068641fd5fbc
ShiffiH <a548487473@gmail.com>
2025-09-25 14:25:39 +0300

    sorry

--- services/db_api_service/app/db.py
+++ services/db_api_service/app/db.py
@@ -1,42 +1,4 @@
-# from dotenv import load_dotenv
-# load_dotenv()
 
-# import os
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from contextlib import contextmanager
-
-# from app.models import Base
-
-# DB_DSN = os.getenv(
-#     "DB_DSN",
-#     "postgresql+psycopg://missions_user:pg123@localhost:5432/missions_db"
-# )
-
-# engine = create_engine(
-#     DB_DSN,
-#     pool_pre_ping=True,
-#     pool_size=10,
-#     max_overflow=20
-# )
-
-# SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
-# @contextmanager
-# def session_scope():
-#     s = SessionLocal()
-#     try:
-#         yield s
-#         s.commit()
-#     except:
-#         s.rollback()
-#         raise
-#     finally:
-#         s.close()
-
-# def init_models():
-#     Base.metadata.create_all(bind=engine)
-# app/db.py
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -44,7 +6,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
-from sqlalchemy.pool import StaticPool  # <— הוסיפי
+from sqlalchemy.pool import StaticPool   
 
 from app.models import Base
 
