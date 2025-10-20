897406efbef6658ebdbf16244fbb878c08a82efd
m0533199321 <m0533199321@gmail.com>
2025-10-20 15:27:57 +0300

    API development to a service #158

--- services/sounds/API-development/src/backend/app.py
+++ services/sounds/API-development/src/backend/app.py
@@ -194,6 +194,10 @@ def delete_schedule(schedule_id):
         cur.close()
         conn.close()
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
